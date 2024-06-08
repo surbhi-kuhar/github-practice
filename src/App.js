603ca3e4c5cd7de6435b9b7bdfbf2097ec66b867
DEEPAKGUPTA1141421 <deepak2198.be21@chitkara.edu.in>
2024-06-08 23:20:52 +0530

    delte everything in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-       delete evry thing and see the response
+       Deepak kumar
     </div>
   );
 }
