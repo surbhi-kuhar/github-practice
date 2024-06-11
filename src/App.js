321bc983b12a1e1829648110e938ce780de9831d
surbhikuhar <surbhikuhar07@gmail.com>
2024-06-11 23:02:47 +0530

    updating app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-
-       Deepak kumar
-
+      <Header />
+      <Footer />
     </div>
   );
 }
